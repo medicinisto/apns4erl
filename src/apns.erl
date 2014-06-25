b3f69d98230de571d0aa8019009077cef4a9089b
Douglas Rapp <doug@layer.com>
2014-06-25 11:44:26 -0700

    expose custom_connection directly

--- src/apns.erl
+++ src/apns.erl
@@ -19,6 +19,7 @@
          send_message/6, send_message/7, send_message/8, send_message/9, send_content/2]).
 -export([estimate_available_bytes/1]).
 -export([message_id/0, expiry/1, timestamp/1]).
+-export([custom_connection/6]).
 
 -type status() :: no_errors | processing_error | missing_token | missing_topic | missing_payload |
                   missing_token_size | missing_topic_size | missing_payload_size | invalid_token |
